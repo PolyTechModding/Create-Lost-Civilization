f7e21aaf88c3fae87479f2384f5382e46eda2352
AwesomeDude091 <computer.cubegeek4life@gmail.com>
2024-03-11 23:51:06 +0100

    Added Dimension

---
+++ fabric/src/main/java/com/polytechmodding/createlostcivilization/fabric/datagen/CivilizationWorldGenerator.java
@@ -0,0 +1,25 @@
+package com.polytechmodding.createlostcivilization.fabric.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CivilizationWorldGenerator extends FabricDynamicRegistryProvider {
+    public CivilizationWorldGenerator(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(HolderLookup.Provider registries, Entries entries) {
+        entries.addAll(registries.lookupOrThrow(Registries.DIMENSION_TYPE));
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "World Generation";
+    }
+}
--- fabric/src/main/java/com/polytechmodding/createlostcivilization/fabric/datagen/DatagenEntrypoint.java
+++ fabric/src/main/java/com/polytechmodding/createlostcivilization/fabric/datagen/DatagenEntrypoint.java
@@ -1,12 +1,22 @@
 package com.polytechmodding.createlostcivilization.fabric.datagen;
 
 import com.arcaneengineering.arcanelib.fabric.datagen.ArcaneDataGenerator;
+import com.polytechmodding.createlostcivilization.CreateLostCivilization;
+import com.polytechmodding.createlostcivilization.world.level.dimension.CivilizationDimensions;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
 
 public class DatagenEntrypoint implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        ArcaneDataGenerator.onInitializeDataGenerator(fabricDataGenerator, null);
+        // ArcaneDataGenerator.onInitializeDataGenerator(fabricDataGenerator, CreateLostCivilization.CONTEXT);
+        fabricDataGenerator.createPack().addProvider(CivilizationWorldGenerator::new);
+    }
+
+    @Override
+    public void buildRegistry(RegistrySetBuilder registryBuilder) {
+        registryBuilder.add(Registries.DIMENSION_TYPE, CivilizationDimensions::bootstrapType);
     }
 }
