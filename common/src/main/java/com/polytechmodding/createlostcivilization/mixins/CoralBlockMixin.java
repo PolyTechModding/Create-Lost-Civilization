5d74c3a570622f70e6b2b05de317f232e303f5d4
AwesomeDude091 <58686898+AwesomeDude091@users.noreply.github.com>
2024-03-11 14:34:05 +0100

    Coral Reef

---
+++ common/src/main/java/com/polytechmodding/createlostcivilization/mixins/CoralBlockMixin.java
@@ -0,0 +1,8 @@
+package com.polytechmodding.createlostcivilization.mixins;
+
+import net.minecraft.world.level.block.CoralBlock;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(CoralBlock.class)
+public class CoralBlockMixin {
+}
