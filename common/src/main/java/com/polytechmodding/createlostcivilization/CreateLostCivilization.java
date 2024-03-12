c140539b67f7616a7976ec088e7e73785a982f85
ClansCoolCoding <fischer.basil@gmail.com>
2024-03-12 21:17:20 +0100

    added Kelp blocks

--- common/src/main/java/com/polytechmodding/createlostcivilization/CreateLostCivilization.java
+++ common/src/main/java/com/polytechmodding/createlostcivilization/CreateLostCivilization.java
@@ -1,6 +1,7 @@
 package com.polytechmodding.createlostcivilization;
 
 import com.arcaneengineering.arcanelib.ArcaneLib;
+import com.arcaneengineering.arcanelib.context.RegistrationContext;
 import com.polytechmodding.createlostcivilization.families.BlockFamiliesFactory;
 import net.minecraft.world.level.block.Blocks;
 
@@ -9,9 +10,10 @@ import java.util.ArrayList;
 public class CreateLostCivilization
 {
 	public static final String MOD_ID = "create_lost_civilization";
+	public static RegistrationContext CONTEXT;
 
 	public static void init() {
 		ArcaneLib arcaneLib = ArcaneLib.getInstance();
-		arcaneLib.apiInit(MOD_ID, new BlockFamiliesFactory(), new ArrayList<>());
+		CONTEXT = arcaneLib.apiInit(MOD_ID, new BlockFamiliesFactory(), new ArrayList<>());
 	}
 }
