d890dc9c8eada0817d8f70ec43cd197700a1af5b
AwesomeDude091 <58686898+AwesomeDude091@users.noreply.github.com>
2024-03-13 21:46:20 +0100

    Reverted accidental kelp merger

--- common/src/main/java/com/polytechmodding/createlostcivilization/families/BlockFamiliesFactory.java
+++ common/src/main/java/com/polytechmodding/createlostcivilization/families/BlockFamiliesFactory.java
@@ -10,52 +10,6 @@ import net.minecraft.world.level.material.MapColor;
 public final class BlockFamiliesFactory implements BlockFamilies.BlockFamiliesFactory {
     @Override
     public BlockFamilies create(RegistrationContext registrationContext) {
-        BlockFamilies blockFamilies = new BlockFamilies(registrationContext);
-
-        registerKelpWoodType(blockFamilies, registrationContext, "blue", MapColor.COLOR_BLUE);
-
-        registerKelpWoodType(blockFamilies, registrationContext, "red", MapColor.COLOR_RED);
-
-        registerKelpWoodType(blockFamilies, registrationContext, "orange", MapColor.COLOR_ORANGE);
-
-        registerKelpWoodType(blockFamilies, registrationContext, "pink", MapColor.COLOR_PINK);
-
-        registerKelpWoodType(blockFamilies, registrationContext, "yellow", MapColor.COLOR_YELLOW);
-
-        return blockFamilies;
-    }
-
-    private static void registerKelpWoodType(BlockFamilies blockFamilies, RegistrationContext registrationContext, String color, MapColor MC){
-        blockFamilies.register(blockFamilies
-                .getOrganicBuilder(
-                        BlockFamilies.getOverworldWoodType(registrationContext, color+"_kelp"),
-                        MC, color+"_kelp", BuiltinDimensionTypes.OVERWORLD, true)
-                        .addWoodBlock(Variants.LOG)
-                        .addWoodBlock(Variants.STRIPPED_LOG)
-                        .addWoodBlock(Variants.WOOD)
-                        .addWoodBlock(Variants.STRIPPED_WOOD)
-
-                        .addWoodBlock(Variants.BOOKSHELF)
-
-                        .addWoodBlock(Variants.HANGING_SIGN)
-                        .addWoodBlock(Variants.SIGN)
-                        .addWoodBlock(Variants.WALL_HANGING_SIGN)
-                        .addWoodBlock(Variants.WALL_SIGN)
-
-                        .addBlock(Variants.STAIRS)
-                        .addBlock(Variants.SLAB)
-
-                        .addItem(Variants.BOAT)
-                        .addItem(Variants.CHEST_BOAT)
-
-                        .addWoodBlock(Variants.FENCE)
-                        .addWoodBlock(Variants.FENCE_GATE)
-
-                        .addWoodBlock(Variants.DOOR)
-                        .addWoodBlock(Variants.TRAPDOOR)
-
-                        .addWoodBlock(Variants.BUTTON)
-                        .addWoodBlock(Variants.PRESSURE_PLATE)
-                );
+        return new BlockFamilies(registrationContext);
     }
 }
